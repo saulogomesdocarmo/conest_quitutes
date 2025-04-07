2d756e09a85b34265920c4240b2bf4e72af4821e
Saulo gomes <saulogcarmo@gmail.com>
2025-04-07 20:41:43 -0300

    conest

---
+++ main.js
@@ -0,0 +1,28 @@
+/**Processo Principal  */
+
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
